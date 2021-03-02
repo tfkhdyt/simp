f50de4c911abda3eb0ab37ec297534f82eb41938
tfkhdyt <ta142fa@gmail.com>
2021-03-03 05:07:27 +0700

    Initial Release

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+clear
+mv simp /data/data/com.termux/files/usr/bin/
+echo "Instalasi sukses!"
+echo "Ketikkan 'simp nama' lalu enter"
+echo "Maka dengan otomatis copypasta simp mu akan tersalin ke clipboard"
+cd ~
+sleep 5
